283002a74cbd02a2663afc6d5d2ebd541884b294
Sathvik-1234 <gandhamunenisathvik@gmail.com>
2026-07-16 09:57:07 +0530

    3rd commit

---
+++ frontend/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+    return ( <
+        div className = "App"
+        style = {
+            { textAlign: 'center', marginTop: '50px', fontFamily: 'Arial' } } >
+        <
+        h1 > DevOps Readiness App < /h1> <
+        p > This app is ready
+        for Docker, Kubernetes, and CI / CD pipelines. < /p> <
+        /div>
+    );
+}
+
+export default App;
